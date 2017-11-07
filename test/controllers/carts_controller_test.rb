9639f7ef675506629f85ceb64d230a948668ed65
Vladimir Zhilyaev <vladimir@zhilyaev.kyiv.ua>
2017-11-07 22:42:00 +0200

    Smarter and more secured cart

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class CartsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @cart = carts(:one)
+    post line_items_url, params: { product_id: products(:ruby).id }
+    @cart = Cart.find(session[:cart_id])
   end
 
   test "should get index" do
@@ -39,10 +40,11 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy cart" do
+
     assert_difference('Cart.count', -1) do
       delete cart_url(@cart)
     end
 
-    assert_redirected_to carts_url
+    assert_redirected_to store_index_url
   end
 end
