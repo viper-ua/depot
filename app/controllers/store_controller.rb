e4a0222382a56f8a09f98ad0da9b32c8382fe0fa
Vladimir Zhilyaev <vladimir@zhilyaev.kyiv.ua>
2017-11-07 16:19:46 +0200

    Adding cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+    session[:counter] ||= 0
+    session[:counter] += 1
+    @message_to_user = nil
+    @message_to_user = "(Add something finally to cart instead of reloading page #{session[:counter]} times)" if session[:counter] > 5
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
@@ -10,4 +12,13 @@ class Product < ApplicationRecord
 		minimum: 10,
 		too_short: "too short, minimum is %{count}"
 	}
+
+	private
+
+		def ensure_not_referenced_by_any_line_item
+			unless line_items.empty?
+				errors.add(:base, 'Lines Items Present')
+				throw :abort
+			end
+		end
 end
