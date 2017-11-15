61418ed5edc609e66435d8bace43033176fc2fd1
Vladimir Zhilyaev <vladimir@zhilyaev.kyiv.ua>
2017-11-15 11:55:28 +0200

    Checkout & Adding mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,16 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'zhilyaev.kyiv.ua',
+    user_name:            'vzhilyaev@gmail.com',
+    password:             'enlhvndsmzgcmfhl',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
