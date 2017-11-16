65e39b076637756721ffefcdcacdff5d8b2b1bd4
Vladimir Zhilyaev <vladimir@zhilyaev.kyiv.ua>
2017-11-16 17:41:38 +0200

    Logging in (Basic Authorization)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, if: Proc.new { |c| c.request.format != 'application/json' }
+  before_action :authorize
+
+  protected
+
+    def authorize
+      unless (session[:user_id] == 0 && User.count == 0) || User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorize
+
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:name])
+    if User.count == 0 || user&.authenticate(params[:password])
+      session[:user_id] = user&.id || 0
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalid user/password combination"
+    end 
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to store_index_url, notice: "Logged out"
+  end
+end
